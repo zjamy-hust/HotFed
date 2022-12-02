59a08894f691fd8919c96daefb4d74a872e8a4f3
Juan Zhao <juan.j.zhao@intel.com>
2022-12-01 23:06:05 -0800

    add showimg to diable/enable showimg

--- xai.py
+++ xai.py
@@ -1,13 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-## %matplotlib inline
+# %matplotlib inline
 
 import torch
 import captum
 import torchvision
 import torchvision.transforms as transforms
-import torchvision.transforms.functional as TF
+import torchvision.transforms.functional as fn
 
 from torchvision import models
 
@@ -20,7 +20,6 @@ from captum.attr import visualization as viz
 from torchvision.utils import make_grid
 from torchvision.io import read_image
 from pathlib import Path
-import torchvision.transforms.functional as fn
 
 from numpy import ndarray
 from typing import Any, Iterable, List, Tuple, Union
@@ -38,6 +37,7 @@ import copy
 args = args_parser()
 args.gpu=3
 device = (f'cuda:{str(args.gpu)}')  if torch.cuda.is_available() else 'cpu'
+showimg=0
 
 
 model_path="/workspace/externalhome/XAI/HotFed/save_checkpoints/xai_analysis/global.iid1.16_15.pth.tar"
@@ -100,36 +100,6 @@ class BasicBlock(nn.Module):
         return out
 
 
-# class Bottleneck(nn.Module):
-#     expansion = 4
-
-#     def __init__(self, in_planes, planes, stride=1):
-#         super(Bottleneck, self).__init__()
-#         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
-#         self.bn1 = nn.BatchNorm2d(planes)
-#         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
-#                                stride=stride, padding=1, bias=False)
-#         self.bn2 = nn.BatchNorm2d(planes)
-#         self.conv3 = nn.Conv2d(planes, self.expansion *
-#                                planes, kernel_size=1, bias=False)
-#         self.bn3 = nn.BatchNorm2d(self.expansion*planes)
-
-#         self.shortcut = nn.Sequential()
-#         if stride != 1 or in_planes != self.expansion*planes:
-#             self.shortcut = nn.Sequential(
-#                 nn.Conv2d(in_planes, self.expansion*planes,
-#                           kernel_size=1, stride=stride, bias=False),
-#                 nn.BatchNorm2d(self.expansion*planes)
-#             )
-
-#     def forward(self, x):
-#         out = F.relu(self.bn1(self.conv1(x)))
-#         out = F.relu(self.bn2(self.conv2(out)))
-#         out = self.bn3(self.conv3(out))
-#         out += self.shortcut(x)
-#         out = F.relu(out)
-#         return out
-
 
 class ResNet(nn.Module):
     def __init__(self, block, num_blocks, num_classes=100):
@@ -177,17 +147,6 @@ apply_transform = transforms.Compose(
             [transforms.ToTensor(),
              transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
 
-# trainset = torchvision.datasets.CIFAR10(root=data_path, train=True,
-#                                         download=True, transform=transform)
-# trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
-#                                           shuffle=True, num_workers=2)
-
-testset = torchvision.datasets.CIFAR10(root=data_path, train=False,
-                                       download=True, transform=apply_transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=32,
-                                         shuffle=False, num_workers=2)
-dataiter = iter(testloader)
-_, labels_workaround = dataiter.next()
 
 classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
@@ -196,8 +155,6 @@ classes = ('plane', 'car', 'bird', 'cat',
 
 path = str(Path(asset_path)/'folder')
 print(path)
-mean = 120.707
-std = 64.15
 files = os.listdir(path)
 d = []
 
@@ -212,60 +169,24 @@ net.to(device)
 net.eval()
 
 
-
-
-
-
-# test_inference(args, net, test_dataset=testset)
-
-
-
 i=0;
 # classes = ('plane' 0, 'car' 1, 'bird' 2, 'cat' 3,
 #            'deer' 4, 'dog' 5, 'frog' 6, 'horse' 7, 'ship' 8, 'truck' 9)
 XAI_labels=[7, 8, 2, 2, 0, 5, 7, 9, 2, 8, 8, 2, 8, 2, 5, 8, 0, 7, 5, 5,1,1,3,3,4,4,6,6,9,3 ]
 for im_name in files:
     if im_name.endswith('.jpg'):
-#     if im_name.endswith('.jpg') and i == 0:
         # if i>1:
         #     break
         i=i+1
         print(str(Path(path)/im_name))
         im_temp=read_image(str(Path(path)/im_name))
         
-        fig, (orig, mask, attr, attr_mask, attr_outmask) = plt.subplots(1, 5)
-        orig.axis('off')
-        mask.axis('off')
-        attr.axis('off')
-        attr_mask.axis('off')
-        attr_outmask.axis('off')
-
-
         original_image_temp = fn.resize(im_temp, size=[32,32])/255
         input_temp=torch.tensor(original_image_temp.unsqueeze(0).cpu().detach().numpy())
         input_temp.requires_grad = True
         img = original_image_temp     # unnormalize
         npimg = img.numpy()
         
-        orig.imshow(np.transpose(npimg, (1, 2, 0)))
-#         plt.axis('off')
-
-
-        # # transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
-        # import numpy as np
-        # from PIL import Image
-        # MEAN = torch.tensor(np.array([0.4914, 0.4822, 0.4465]))
-        # STD = torch.tensor(np.array([0.2023, 0.1994, 0.2010]))
-        # # img_pil = Image.open("ty.jpg")
-        # # x = np.array(img_pil)
-        # # input_temp = input_temp.transpose(-1, 0, 1)
-        # # input_temp = (input_temp - MEAN) / STD
-        # torch.nn.functional.normalize(input_temp,MEAN,STD)
-
-
-#         original_image_temp = np.transpose(original_image_temp, (1, 2, 0))
-
-        import torchvision.transforms.functional as fn
         input_temp_norm=fn.normalize(input_temp, mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010])
 
         input_temp_norm=input_temp_norm.to(device)
@@ -284,8 +205,6 @@ for im_name in files:
 
         ig = IntegratedGradients(net)
         nt = NoiseTunnel(ig)
-        # labels_workaround[0] = XAI_labels[int(im_name[:-4])-1]
-        # attr_ig_nt = attribute_image_features(nt, input_temp, label=predicted_temp[0], baselines=input_temp * 0, nt_type='smoothgrad_sq',  nt_samples=100, stdevs=0.2)
         input_temp=input_temp.to(device)
         attr_ig_nt = attribute_image_features(nt, input_temp,truth=XAI_labels[int(im_name[:-4])-1], label=predicted_temp[0], baselines=input_temp * 0, nt_type='smoothgrad_sq',  nt_samples=100, stdevs=0.2)
         attr_ig_nt = np.transpose(attr_ig_nt.squeeze(0).cpu().detach().numpy(), (1, 2, 0))
@@ -297,165 +216,32 @@ for im_name in files:
         threshold = _cumulative_sum_threshold(attr_combined, 100 - outlier_perc)
         attr_norm = attr_combined / threshold
         attr_norm=attr_norm*(attr_norm>0.3)
-        default_cmap = LinearSegmentedColormap.from_list(
-            "RdWhGn", ["red", "white", "green"]
-        )
-        vmin, vmax = -1, 1
-        attr.imshow(attr_norm,cmap=default_cmap,vmin=vmin,vmax=vmax)
-        mask.imshow(truth_mask,cmap="Blues",vmin=0,vmax=1)
-
+        
         masked = cv2.add(attr_norm, np.zeros(np.shape(attr_norm), dtype=float), mask=truth_mask) 
         out_mask = cv2.add(attr_norm, np.zeros(np.shape(attr_norm), dtype=float), mask=255-truth_mask) 
 
-
-        attr_mask.imshow(masked,cmap="Greens",vmin=0,vmax=1)
         pixelnum=np.count_nonzero(masked)
         print("in mask pixelnum", pixelnum,pixelnum_all,pixelnum/pixelnum_all)
         out_pixelnum=np.count_nonzero(out_mask)
         print("out mask pixelnum", out_pixelnum,3*32*32-pixelnum_all, out_pixelnum/(3*32*32-pixelnum_all))
-        attr_outmask.imshow(out_mask,cmap="Reds",vmin=0,vmax=1)
-        fig.show()
-
-
-
-
-# criterion = nn.CrossEntropyLoss()
-# optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-
-
-# checkpoint = torch.load(model_path)
-# net.load_state_dict(checkpoint['state_dict'])
-
-
-
-
-# dataiter = iter(testloader)
-# images, labels = dataiter.next()
-
-# # print images
-# imshow(torchvision.utils.make_grid(images))
-# print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(32)))
-
-
-# outputs = net(images)
-
-# _, predicted = torch.max(outputs, 1)
-
-# print('Predicted: ', ' '.join('%5s' % classes[predicted[j]]
-#                               for j in range(32)))
-
-
-
-# dog1_int = read_image(str(Path(asset_path) / 'dog1_32.jpg'))
-
-# original_image_temp = fn.resize(dog1_int, size=[32,32])/255
-
-# dog1_mask = read_image(str(Path(asset_path) / 'dog1_32_mask.png'),mode=torchvision.io.image.ImageReadMode.RGB)
-
-# input_temp=torch.tensor(original_image_temp.unsqueeze(0).float())
-# input_temp.requires_grad = True
-
-# plt.rcParams["savefig.bbox"] = 'tight'
-
-
-# output_temp = net(input_temp)
-
-# _, predicted_temp = torch.max(output_temp, 1)
-# print(predicted_temp)
-
-# net.eval()
-
-
         
-
-# saliency = Saliency(net)
-# # grads = saliency.attribute(input, target=labels[ind].item())
-# grads = saliency.attribute(input_temp, target=5)
-# grads = np.transpose(grads.squeeze().cpu().detach().numpy(), (1, 2, 0))
-
-
-# ig = IntegratedGradients(net)
-# ind=0
-# labels[ind]=5
-# attr_ig, delta = attribute_image_features(ig, input_temp, baselines=input_temp * 0, return_convergence_delta=True)
-# attr_ig = np.transpose(attr_ig.squeeze().cpu().detach().numpy(), (1, 2, 0))
-# print('Approximation delta: ', abs(delta))
-
-
-# ig = IntegratedGradients(net)
-# nt = NoiseTunnel(ig)
-# attr_ig_nt = attribute_image_features(nt, input_temp, baselines=input_temp * 0, nt_type='smoothgrad_sq',
-#                                       nt_samples=100, stdevs=0.2)
-# attr_ig_nt = np.transpose(attr_ig_nt.squeeze(0).cpu().detach().numpy(), (1, 2, 0))
-
-# dl = DeepLift(net)
-# attr_dl = attribute_image_features(dl, input_temp, baselines=input_temp * 0)
-# attr_dl = np.transpose(attr_dl.squeeze(0).cpu().detach().numpy(), (1, 2, 0))
-
-# print('Original Image')
-# print('Predicted:', classes[predicted_temp[0]], 
-#       ' Probability:', torch.max(F.softmax(outputs, 1)).item())
-
-# original_image = np.transpose((original_image_temp.cpu().detach().numpy() / 2) + 0.5, (1, 2, 0))
-
-# _ = viz.visualize_image_attr(None, original_image, 
-#                       method="original_image", title="Original Image")
-
-# _ = viz.visualize_image_attr(grads, original_image, method="heat_map", sign="absolute_value",
-#                           show_colorbar=True, title="Overlayed Gradient Magnitudes")
-
-# _ = viz.visualize_image_attr(attr_ig, original_image, method="heat_map",sign="all",
-#                           show_colorbar=True, title="Overlayed Integrated Gradients")
-
-# _ = viz.visualize_image_attr(attr_ig_nt, original_image, method="heat_map", sign="absolute_value", 
-#                              outlier_perc=10, show_colorbar=True, 
-#                              title="Overlayed Integrated Gradients \n with SmoothGrad Squared")
-
-# _ = viz.visualize_image_attr(attr_dl, original_image, method="heat_map",sign="all",show_colorbar=True, 
-#                           title="Overlayed DeepLift")
-
-
-
-
-# dog1_mask_tensor = read_image(str(Path(asset_path) / 'dog1_32_mask.png'),mode=torchvision.io.image.ImageReadMode.RGB)
-# dog1_mask = cv2.imread(str(Path(asset_path) / 'dog1_32_mask.png'),cv2.IMREAD_GRAYSCALE)
-# dog1_mask_np=dog1_mask_tensor.numpy()
-# pixelnum_all=np.count_nonzero(dog1_mask_np)
-# print("pixelnum_all", pixelnum_all)
-
-
-
-# outlier_perc = 10
-# attr_combined = np.sum(attr_ig_nt, axis=2)
-# attr_combined = np.abs(attr_combined)
-# threshold = _cumulative_sum_threshold(attr_combined, 100 - outlier_perc)
-# attr_norm = attr_combined / threshold
-# attr_norm=attr_norm*(attr_norm>0.7)
-# default_cmap = LinearSegmentedColormap.from_list(
-#     "RdWhGn", ["red", "white", "green"]
-# )
-# vmin, vmax = -1, 1
-# plt.imshow(attr_norm,cmap=default_cmap,vmin=vmin,vmax=vmax)
-# plt.show()
-# plt.hist(attr_norm)
-# print(attr_norm)
-# plt.show()
-# plt.imshow(dog1_mask,cmap=default_cmap,vmin=vmin,vmax=vmax)
-# plt.show()
-
-# masked = cv2.add(attr_norm, np.zeros(np.shape(attr_norm), dtype=float), mask=dog1_mask) 
-# masked1 = cv2.add(attr_norm, np.zeros(np.shape(attr_norm), dtype=float), mask=255-dog1_mask) 
-
-
-# plt.hist(masked)
-# plt.show()
-# plt.hist(masked1)
-# plt.show()
-# plt.imshow(masked,cmap=default_cmap,vmin=vmin,vmax=vmax)
-# plt.show()
-# pixelnum=np.count_nonzero(masked)
-# print("masked pixelnum", pixelnum,pixelnum/pixelnum_all)
-# pixelnum=np.count_nonzero(masked1)
-# print("masked1 pixelnum", pixelnum/(3*32*32-pixelnum_all))
-# plt.imshow(masked1,cmap=default_cmap,vmin=vmin,vmax=vmax)
-# plt.show()
+        if showimg==1 and i%2 == 1:
+            fig, (orig, mask, attr, attr_mask, attr_outmask) = plt.subplots(1, 5)
+            orig.axis('off')
+            mask.axis('off')
+            attr.axis('off')
+            attr_mask.axis('off')
+            attr_outmask.axis('off')
+            orig.imshow(np.transpose(npimg, (1, 2, 0)))
+            default_cmap = LinearSegmentedColormap.from_list(
+                "RdWhGn", ["red", "white", "green"]
+            )
+            vmin, vmax = -1, 1
+            attr.imshow(attr_norm,cmap=default_cmap,vmin=vmin,vmax=vmax)
+            mask.imshow(truth_mask,cmap="Blues",vmin=0,vmax=1)
+            attr.imshow(attr_norm,cmap=default_cmap,vmin=vmin,vmax=vmax)
+            mask.imshow(truth_mask,cmap="Blues",vmin=0,vmax=1)
+            attr_mask.imshow(masked,cmap="Greens",vmin=0,vmax=1)
+            attr_outmask.imshow(out_mask,cmap="Reds",vmin=0,vmax=1)
+            fig.show()
+        # plt.close(fig)
