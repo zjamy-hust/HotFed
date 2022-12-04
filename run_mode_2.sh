19b099e13e60692528c0e7303467d4dc6c008aa5
Juan Zhao <juan.j.zhao@intel.com>
2022-12-04 07:27:24 -0800

    yuankai: add new file

---
+++ run_mode_2.sh
@@ -0,0 +1,6 @@
+mode=2
+
+python -u fedxai.py --epochs 20 --num_users 16 \
+                    --local_ep 50 --gpu 1 --iid 1 --mode $mode \
+                    --dataset 'cifar10' --num_classes 10 \
+                    > ./res/mode_${mode}.log 2>&1
