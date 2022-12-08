9016a7ecb7d2dd6e12e058cb368960644634ac3e
Juan Zhao <juan.j.zhao@intel.com>
2022-12-08 01:00:57 -0800

    add missing MNIST files

--- models/models_resnet.py
+++ models/models_resnet.py
@@ -71,11 +71,11 @@ class Bottleneck(nn.Module):
 
 
 class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=100):
+    def __init__(self, block, num_blocks, num_classes=100, input_channel=3):
         super(ResNet, self).__init__()
         self.in_planes = 64
 
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
+        self.conv1 = nn.Conv2d(input_channel, 64, kernel_size=3,
                                stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
@@ -107,6 +107,8 @@ class ResNet(nn.Module):
 def ResNet18():
     return ResNet(BasicBlock, [2, 2, 2, 2])
 
+def ResNet18_MNIST():
+    return ResNet(BasicBlock, [2, 2, 2, 2], input_channel=1)
 
 def ResNet34():
     return ResNet(BasicBlock, [3, 4, 6, 3])
